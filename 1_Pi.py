09352efab413e8b3f90d3c60af08c84a541fd796
Kiryl Pashkin <sleembelbox@gmail.com>
2022-07-19 13:41:56 +0300

    modified task1

--- 1_Pi.py
+++ 1_Pi.py
@@ -3,6 +3,9 @@
 # Пример:
 # - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 
+from operator import truediv
+from decimal import *
+
 def get_num_comma(f_num: float) -> int:
     symb = 0
     while(f_num < 1):
@@ -10,7 +13,17 @@ def get_num_comma(f_num: float) -> int:
         f_num *= 10
     return symb
 
-pi = 3.14159265358979323846 # 20 digits
+def pi(n):
+    pi = Decimal(0)
+    k = 0
+    while k < n:
+        pi += (Decimal(1)/(16**k))*((Decimal(4)/(8*k+1))-(Decimal(2)/(8*k+4))-(Decimal(1)/(8*k+5))-(Decimal(1)/(8*k+6)))
+        k += 1
+    return pi
+
+
+
+pi = pi(100) 
 correct = False
 while(not correct):
     try:
